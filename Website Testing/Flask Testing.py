257c614a853426ac850b38b16cb2c096d16ad196
Polarbear071 <gibbisonp@gmail.com>
2024-11-16 16:23:44 +0000

    Fully completing navbar
    Added styling to navbar
    
    Changed navbar slightly so that everything is more cohesive
    
    Added a new 'about' page but no content at the moment
    
    Changed the name of the 'new' page to 'test'

--- Website Testing/Flask Testing.py
+++ Website Testing/Flask Testing.py
@@ -8,8 +8,12 @@ def home():    # defining the home page
     return render_template('index.html')    # Rendering the template within the page
 
 @app.route('/test')
-def new():
-    return render_template('new.html')      # Rendering the template
+def test():
+    return render_template('test.html')      # Rendering the template
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':    # Ensuring that the website wil run
     app.run(debug = True)    # 'debug = True' ensures that any changes are automatically changed upon save not re-run
