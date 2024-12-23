d62d1cad631522a4061c53a34132479bf8f7d7a8
Polarbear071 <gibbisonp@gmail.com>
2024-12-23 21:10:44 +0100

    Favicon and About Page Attempt
    Attempted to add favicon icon
    Attempted to update page.html
    
    Both unsuccessful as of yet

--- Website Development/app.py
+++ Website Development/app.py
@@ -1,5 +1,5 @@
 # Imports
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)    # Standard definition of flask
 
@@ -9,11 +9,11 @@ def home():    # defining the home page
 
 @app.route('/learning')
 def test():
-    return render_template('learning.html')      # Rendering the template
+    return render_template('learning.html') # Rendering the template
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-if __name__ == '__main__':    # Ensuring that the website wil run
+if __name__ == '__main__':   # Ensuring that the website wil run
     app.run(debug = True)    # 'debug = True' ensures that any changes are automatically changed upon save not re-run
