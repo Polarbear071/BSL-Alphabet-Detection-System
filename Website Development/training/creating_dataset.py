d2913c16094ecea76b32c179664754bc096c3084
Polarbear071 <gibbisonp@gmail.com>
2024-12-23 17:25:44 +0100

    Creating the classifier
    Created the classifier (AI) for each letter of the alphabet
    Created the model.p file to store the classifier model

--- Website Development/training/creating_dataset.py
+++ Website Development/training/creating_dataset.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
@@ -12,22 +13,27 @@ hands = mp_hands.Hands(static_image_mode = True, min_detection_confidence = 0.3)
 
 DATA_DIR = './training data'
 
+data = []
+labels = []
+
 for dir_ in os.listdir(DATA_DIR):   # every directory in training data folder
     for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:   # every frame within the directory
+        data_aux = []
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # converting the image for mediapipe and matplotlib
 
         results = hands.process(img_rgb)
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks: # for every result
-                mp_drawing.draw_landmarks(
-                    img_rgb,    # image being drawn
-                    hand_landmarks, # the model being output
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x    # Storing x and y co ordinates
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x)  # Appending x and y co ordinates
+                    data_aux.append(y)
+    
+            data.append(data_aux)
+            labels.append(dir_)
 
-        plt.figure()
-        plt.imshow(img_rgb)
-
-plt.show()
+f = open('data.pickle', 'wb')   # writing the data as bytes
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
---
+++ Website Development/training/train_classifier.py
@@ -0,0 +1,37 @@
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+data_dict = pickle.load(open('./data.pickle', 'rb'))    # Giving pickle the data.pickle file
+
+# Defining data and labels within the data.pickle file
+data = np.asarray(data_dict['data'], dtype = object)
+labels = np.asarray(data_dict['labels'], dtype = object)
+
+# Ensuring all items are of the same size
+def pad_or_truncate(sample, target_length):
+    if len(sample) > target_length:
+        return sample[:target_length]
+    else:
+        return sample + [0] * (target_length - len(sample))
+target_length = max(len(sample) for sample in data_dict['data'])
+data = [pad_or_truncate(sample, target_length) for sample in data_dict['data']]
+
+# Splitting the data into two sets for data and labels
+# Random 20% of the data is set under 'test'
+# Data will be split with the same proportion of each label
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+model = RandomForestClassifier()
+model.fit(x_train, y_train)
+
+y_predict = model.predict(x_test)
+
+score = accuracy_score(y_predict, y_test)
+print('{}% of samples classified correctly'.format(score * 100))
+
+f = open('model.p', 'wb')
+pickle.dump({'model': model}, f)
+f.close()
